d687fa16c88e11bf31c9e2fa02d4fb2c094fc1f4
Riccardo Persello <riccardo.persello@icloud.com>
2023-01-31 14:54:43 +0100

    Added data size checks.

--- Sources/CharacteristicKit/Characteristics/DataDecodable.swift
+++ Sources/CharacteristicKit/Characteristics/DataDecodable.swift
@@ -13,78 +13,110 @@ protocol DataDecodable {
 
 extension Int8: DataDecodable {
   func decode(from data: Data) -> Int8? {
+    guard data.count == 1 else {
+      return nil
+    }
+
     return data.withUnsafeBytes { $0.load(as: Int8.self) }
   }
 }
 
 extension UInt8: DataDecodable {
   func decode(from data: Data) -> UInt8? {
+    guard data.count == 1 else {
+      return nil
+    }
+
     return data.withUnsafeBytes { $0.load(as: UInt8.self) }
   }
 }
 
 extension Int16: DataDecodable {
   func decode(from data: Data) -> Int16? {
+    guard data.count == 2 else {
+      return nil
+    }
+
     return data.withUnsafeBytes { $0.load(as: Int16.self) }
   }
 }
 
 extension UInt16: DataDecodable {
   func decode(from data: Data) -> UInt16? {
+    guard data.count == 2 else {
+      return nil
+    }
+
     return data.withUnsafeBytes { $0.load(as: UInt16.self) }
   }
 }
 
 extension Int32: DataDecodable {
   func decode(from data: Data) -> Int32? {
+    guard data.count == 4 else {
+      return nil
+    }
+
     return data.withUnsafeBytes { $0.load(as: Int32.self) }
   }
 }
 
 extension UInt32: DataDecodable {
   func decode(from data: Data) -> UInt32? {
+    guard data.count == 4 else {
+      return nil
+    }
+
     return data.withUnsafeBytes { $0.load(as: UInt32.self) }
   }
 }
 
 extension Int64: DataDecodable {
   func decode(from data: Data) -> Int64? {
+    guard data.count == 8 else {
+      return nil
+    }
+
     return data.withUnsafeBytes { $0.load(as: Int64.self) }
   }
 }
 
 extension UInt64: DataDecodable {
   func decode(from data: Data) -> UInt64? {
+    guard data.count == 8 else {
+      return nil
+    }
+
     return data.withUnsafeBytes { $0.load(as: UInt64.self) }
-  }
-}
-
-extension Int: DataDecodable {
-  func decode(from data: Data) -> Int? {
-    return data.withUnsafeBytes { $0.load(as: Int.self) }
-  }
-}
-
-extension UInt: DataDecodable {
-  func decode(from data: Data) -> UInt? {
-    return data.withUnsafeBytes { $0.load(as: UInt.self) }
   }
 }
 
 extension Float16: DataDecodable {
   func decode(from data: Data) -> Float16? {
+    guard data.count == 2 else {
+      return nil
+    }
+
     return data.withUnsafeBytes { $0.load(as: Float16.self) }
   }
 }
 
 extension Float32: DataDecodable {
   func decode(from data: Data) -> Float32? {
+    guard data.count == 4 else {
+      return nil
+    }
+
     return data.withUnsafeBytes { $0.load(as: Float32.self) }
   }
 }
 
 extension Float64: DataDecodable {
   func decode(from data: Data) -> Float64? {
+    guard data.count == 8 else {
+      return nil
+    }
+
     return data.withUnsafeBytes { $0.load(as: Float64.self) }
   }
 }
